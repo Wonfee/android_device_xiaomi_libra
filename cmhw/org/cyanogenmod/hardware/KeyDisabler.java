2d084b5c9ecc6194fce4d884060e269d20dea589
crancocco <shulaibai@hustunique.com>
2016-04-20 13:11:44 +0800

    update KeyDisabler

--- cmhw/org/cyanogenmod/hardware/KeyDisabler.java
+++ cmhw/org/cyanogenmod/hardware/KeyDisabler.java
@@ -29,7 +29,7 @@ import org.cyanogenmod.hardware.util.FileUtils;
 
 public class KeyDisabler {
 
-    private static String GPIO_CONTROL_PATH = "/sys/devices/soc.0/gpio_keys.88/disabled_keys";
+    private static String GPIO_CONTROL_PATH = "/sys/devices/soc.0/f9924000.i2c/i2c-2/2-0070/input/input1/0dbutton";
 
     public static boolean isSupported() { return true; }
 
